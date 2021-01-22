d4989e0ee6beb15d50ea7ba217933812dcd5d7d2
Travis0202 <travis.witts@hotmail.co.uk>
2021-01-22 15:33:39 +1030

    trying to fix async

--- script.js
+++ script.js
@@ -6,31 +6,38 @@ var currentDay = []
 
 var icon = "http://openweathermap.org/img/wn/" + "03n" + ".png"
 
-var  latLon = $.ajax({
-    url: url,
-    method: "GET"
-}).then(function(response) {
-    console.log(response)
-    return response.coord
-})
-console.log(currentDay)
-const url2 = "http://api.openweathermap.org/data/2.5/onecall?lat=" +latLon[0]+ "&lon=" +latLon[1]+ "&appid=" + key
-$.ajax({
-    url : url2,
-    method: "GET"
-}).then(function(response) {
-    console.log(response)
-    var fiveDayForecast = []
-    for (i= 1; i < 6; i++) {
-        var datObj = {day: i, icon: response.daily[i].weather[0].icon, temp: response.daily[i].temp}
-    }
-})
-
-
+function currentDay() {
+    var lat = $.ajax({
+        url: url,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response)
+        return response.coord
+    })
+    return lat
+}
+async function fiveDay() {
+    var latLon = await currentDay()
+    const url2 = "http://api.openweathermap.org/data/2.5/onecall?lat=" +latLon[0]+ "&lon=" +latLon[1]+ "&appid=" + key
+    var forecast = $.ajax({
+        url : url2,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response)
+        var fiveDayForecast = []
+        for (i= 1; i < 6; i++) {
+            var datObj = {day: i, icon: response.daily[i].weather[0].icon, temp: response.daily[i].temp}
+        }
+        return fiveDayForecast
+    })
+    return forecast
+}
+console.log(fiveDay())
 
 
 
 function displayCurrent(city, currentWeather, time) {
+    
     var cityName = $("#city-name")
     $("#city-name").text(city);
     $("#current-date").text(" " + time + " ");
