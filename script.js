85e207b08ccbf97ddd77442713995afed31680b1
Travis0202 <travis.witts@hotmail.co.uk>
2021-01-24 23:00:34 +1030

    added historyBuilder func

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
+var historyCities = JSON.parse(localStorage.getItem("historyArray")) || []
 
 
-
-var city  = ""
 
 function currentWeather(cityName) {
     const key1 = '41027e464b9989a936092c6f0b19cbe3'
@@ -21,12 +20,12 @@ function currentWeather(cityName) {
         $("#uv-index").text()
         const key3 = '41027e464b9989a936092c6f0b19cbe3'
         const url3 = "http://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord[0] + "&lon=" + response.coord[1] + "&appid=" + key3
-        $.ajax({
-            url: url3,
-            method: "GET"
-        }).then(function(response) {
-            console.log(response)
-        })
+        // $.ajax({
+        //     url: url3,
+        //     method: "GET"
+        // }).then(function(response) {
+        //     console.log(response)
+        // })
     })
 
 }
@@ -37,30 +36,30 @@ function forecast(cityName) {
         url: url2,
         method: "GET"
     }).then(function (response) {
-     var forecastList= []
-     for (i=0; i < 40; i++) {
-         var time = response.list[i].dt_txt
-         if (time.indexOf("12") == 11) {
-             forecastList.push(response.list[i])
-         }
-     }
-     var forecastCont = $(".five-day-forecast")
-     forecastCont.empty()
-     $("#current-date").text(" " + forecastList[0].dt_txt.substring(8, 10) + "/" + forecastList[0].dt_txt.substring(5, 7) + " ");
-     for (i= 0; i < 5; i++) {
-        var forecastCol = $("<div>").addClass("col-md-2");
-        var forecastCard = $("<div>").addClass("card text-white bg-dark mb-3")
-        forecastCard.css("max-width", "18rem")
-        forecastCard.css("height", "200px")
-        forecastCard.append($("<h5>").text(forecastList[i].dt_txt.substring(8, 10) + "/" + forecastList[i].dt_txt.substring(5, 7)))
-        var iconURL = "https://openweathermap.org/img/wn/" + forecastList[i].weather[0].icon + ".png"
-        forecastCard.append($("<img>").attr("src", iconURL))
-        temperature = forecastList[i].main.temp - 273.15
-        forecastCard.append($("<p>").text("Temp: " + temperature.toFixed(1) + "C"))
-        forecastCard.append($("<p>").text("Humidity: " + forecastList[i].main.humidity + "%"))
-        forecastCont.append(forecastCol)
-        forecastCol.append(forecastCard)
-     }
+        var forecastList = []
+        for (i = 0; i < 40; i++) {
+            var time = response.list[i].dt_txt
+            if (time.indexOf("12") == 11) {
+                forecastList.push(response.list[i])
+            }
+        }
+        var forecastCont = $(".five-day-forecast")
+        forecastCont.empty()
+        $("#current-date").text(" " + forecastList[0].dt_txt.substring(8, 10) + "/" + forecastList[0].dt_txt.substring(5, 7) + " ");
+        for (i = 0; i < 5; i++) {
+            var forecastCol = $("<div>").addClass("col-md-2");
+            var forecastCard = $("<div>").addClass("card text-white bg-dark mb-3")
+            forecastCard.css("max-width", "18rem")
+            forecastCard.css("height", "200px")
+            forecastCard.append($("<h5>").text(forecastList[i].dt_txt.substring(8, 10) + "/" + forecastList[i].dt_txt.substring(5, 7)))
+            var iconURL = "https://openweathermap.org/img/wn/" + forecastList[i].weather[0].icon + ".png"
+            forecastCard.append($("<img>").attr("src", iconURL))
+            temperature = forecastList[i].main.temp - 273.15
+            forecastCard.append($("<p>").text("Temp: " + temperature.toFixed(1) + "C"))
+            forecastCard.append($("<p>").text("Humidity: " + forecastList[i].main.humidity + "%"))
+            forecastCont.append(forecastCol)
+            forecastCol.append(forecastCard)
+        }
     })
 }
 
@@ -69,35 +68,41 @@ function getDate(timezone) {
     var time = moment().utcOffset(offset).format('LL')
     return time
 }
+function historyBuilder(city) {
+    var historyCities = JSON.parse(localStorage.getItem("historyArray")) || []
+    if (historyCities.indexOf(city) >= 0) {
+        localStorage.setItem("historyArray", JSON.stringify(historyCities))
+    }
+    else {
+        historyCities.splice(0, 0, city);
+        localStorage.setItem("historyArray", JSON.stringify(historyCities))
+    }
+    $(".list-group").empty()
+    for (i = 0; i < historyCities.length; i++) {
+        var historyBTN = $("<a>").addClass("list-group-item list-group-item-action")
+        historyBTN.text(historyCities[i])
+        historyBTN.attr("href", "#")
+        $(".list-group").append(historyBTN)
+    }
+}
 
 async function getUV(cityName) {
-    var city  = cityName;
+    var city = cityName;
+    
 
 }
 
-$(document).on("click", ".searchBTN", function(event) {
+$(document).on("click", ".searchBTN", function (event) {
     event.preventDefault()
     city = $("#city-input").val();
     city = city.charAt(0).toUpperCase() + city.slice(1);
     console.log(city)
-    var historyCities = JSON.parse(localStorage.getItem("history")) || []
-    if (historyCities.indexOf(city) >= 0) {
-        localStorage.setItem("history", JSON.stringify(historyCities))
-    }
-    else {
-        historyCities.splice(0, 0, city);
-        localStorage.setItem("history", JSON.stringify(historyCities))
-        var historyBTN = $("<a>").addClass("list-group-item list-group-item-action")
-        historyBTN.val(city)
-        historyBTN.attr("href", "#")
-        $(".list-group").prepend(historyBTN)
-    }
-
+    historyBuilder(city)
     currentWeather(city)
     forecast(city)
 })
 
-$(".list-group").on("click", ".list-group-item", function(event) {
+$(".list-group").on("click", ".list-group-item", function (event) {
     event.preventDefault();
     city = $(this).text();
     console.log(city)
